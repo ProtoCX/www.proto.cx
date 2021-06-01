6fab1baf79a39cc0895db77707c32ad112d09aa6
Bilal Khan <bilal@proto.cx>
2021-06-01 10:29:51 -0400

    Fixed discountRate not capped for over 50K users

--- roi_calc2.js
+++ roi_calc2.js
@@ -314,11 +314,12 @@ function formatStorageString(dataSelector, data) {
     let totalSavings = 9 * ((data.amch * data.acpch) + (data.amc * data.amcpc) + (data.ame * data.amcpe))
 
     // Proto Pricing
-    let discountRate = (data.acpm <= 5000) ? 1 : (1 - (Math.ceil(data.acpm/10000) * 0.025 ))
-		let baseValue = (data.acpm > 5000) ? 3000 : 1000
-		let userOffset = (data.acpm > 5000) ? 5000 : 1000
-		let pricePerUser = (data.acpm > 5000) ? 0.1 : 0.5
-		let protoPricingPerMonth = Math.round((baseValue + (data.acpm - userOffset) * pricePerUser) * discountRate)
+
+    let discountRate = (data.acpm <= 5000) ? 1 : (1 - (Math.ceil((data.acpm > 50000 ? 50000 : data.acpm)/10000) * 0.025 ))
+	  let baseValue = (data.acpm > 5000) ? 3000 : 1000
+	  let userOffset = (data.acpm > 5000) ? 5000 : 1000
+	  let pricePerUser = (data.acpm > 5000) ? 0.1 : 0.5
+	  let protoPricingPerMonth = Math.round((baseValue + (data.acpm - userOffset) * pricePerUser) * discountRate)
     var protoPricing = protoPricingPerMonth * 12
 
     var roi = ((totalSavings - protoPricing) - protoPricing) / (protoPricing/100)
